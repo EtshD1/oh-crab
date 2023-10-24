39c03fabd68b16749ecb4d884c3fac9678ce74f4
EtshD1 <mohamed.h.abouelenin@gmail.com>
2023-10-24 23:25:38 +0300

    chapter 3: comments

---
+++ chp_3/_3_4_comments/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Hello world. This is a comments, which is ignored by the compiler
+    // and is only for developers to read for whatever reason they need
+
+    println!("Hello, Comments!"); // Comments can also be placed after code
+    // But mostly comments will on a seperate line like this
+    println!("Nothing to see here, just the code...");
+}
